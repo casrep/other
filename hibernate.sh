40afc7983320ecf8ade0688ae22a474167e9b16d
casrep <derek.jansen@savvis.com>
2014-09-17 12:56:43 -0500

    Updated hibernate script to echo results.

--- hibernate.sh
+++ hibernate.sh
@@ -1,13 +1,13 @@
-echo "(Are you sure?) "
-read -e answer
-    for response in y Y yes YES Yes
-    do
-        if [ "_$answer" == "_$response" ]
-        then
-            return 0
-        fi
-    done
-dbus-send --system --print-reply \
-    --dest="org.freedesktop.UPower" \
-    /org/freedesktop/UPower \
-    org.freedesktop.UPower.Hibernate
+echo "     \"Are you sure?\""
+read answer
+for response in y Y yes YES Yes; do
+    if [ "$answer" = "$response" ]; then
+        echo "     \"Running...\""
+        dbus-send --system --print-reply \
+            --dest="org.freedesktop.UPower" \
+            /org/freedesktop/UPower \
+            org.freedesktop.UPower.Hibernate
+        return 0 &> /dev/null
+    fi
+done
+echo "     \"Done.\""
